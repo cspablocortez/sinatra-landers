e32a45c88a35b11c08d329292878a15f25a33b5c
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-05-07 18:04:03 -0700

    End of May 7 class
    Learned Sinatra project structure and ERB views.

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
 
-get '/' do                  # root element "http://localhost/"
-    'Hello, world!'
+get '/' do                                  # root element "http://localhost/"
+    erb :hello, layout: true
 end
 
 get '/hello/:name' do             
     erb 
 end
+
 
